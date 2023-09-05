f7fa2554889e154c1cd73d31851150371e9514a3
chiranjivn <chiranjeevnag@gmail.com>
2023-09-05 19:42:15 +0530

    new pop-up window in counselling page post request in server

--- server.js
+++ server.js
@@ -1,14 +1,44 @@
 const express = require('express');
 const handleBars = require('express-handlebars');
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 const path = require('path');
 
 app.engine('handlebars', handleBars.engine());
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 app.set('view engine', 'handlebars');
 app.set('views', './views');
 
 app.use(express.static(__dirname+'/public'));
+
+
+
+
+const ContactSchema = new mongoose.Schema({
+  name: String,
+  phone: Number,
+  email: String,
+  address: String,
+  purpose: String,
+});
+
+const Contact = mongoose.model("Contact", ContactSchema);
+const dbURI = process.env.MONGODB_URI;
+mongoose
+  .connect(dbURI)
+  .then((res) => {
+    console.log("Connected to Database.");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+
 
 app.get('/', (req, res) => {
     res.render('home',{
@@ -50,6 +80,18 @@ app.get('/about-Us',(req,res)=>{
 // app.get('/home',(req,res)=>{
 //     res.sendFile(path.join(__dirname,'/','home/index.html'));
 // })
+
+app.post("/contact", async (req, res) => {
+    const data = req.body;
+    const contact = await Contact.create({
+      name: data.name,
+      phone: data.phone,
+      email: data.email,
+      address: data.address,
+      purpose: data.purpose,
+    });
+    console.log(contact);
+  });
 
 app.listen(port,()=>{
     console.log(`Example app listening on port ${port}`)
